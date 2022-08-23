815405533248d2a40b56f5ddfeaf7a84ec808aaf
Anahi <cosmos.anahi@gmail.com>
2022-08-22 22:28:51 -0300

    Varios cambios
    Cambios en el frontend & en el backend

---
+++ proyectoFinal_LedesmaAnahi/BackEnd/src/main/java/com/portfolio/mlab/MlabApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.mlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MlabApplication.class, args);
+	}
+
+}

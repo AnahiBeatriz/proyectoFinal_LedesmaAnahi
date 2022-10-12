bc2fea7cbbb4906249da25c0eeb87cccaffbdbab
Anahi <cosmos.anahi@gmail.com>
2022-10-12 11:20:42 -0300

    componentes modificados
    modifico componentes educación y experiencia laboral

--- proyectoFinal_LedesmaAnahi/FrontEnd/src/app/app.module.ts
+++ proyectoFinal_LedesmaAnahi/FrontEnd/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -18,6 +18,10 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { FormsModule } from '@angular/forms';
 import { interceptorProvider } from './service/interceptor-service';
+import { NewExperienciaComponent } from './components/experiencia/new-experiencia/new-experiencia.component';
+import { EditExperienciaComponent } from './components/experiencia/edit-experiencia/edit-experiencia.component';
+import { NeweducacionComponent } from './components/educacion/neweducacion/neweducacion.component';
+import { EditeducacionComponent } from './components/educacion/editeducacion/editeducacion.component';
 
 @NgModule({
   declarations: [
@@ -32,11 +36,16 @@ import { interceptorProvider } from './service/interceptor-service';
     ProyectoComponent,
     FooterComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    NewExperienciaComponent,
+    EditExperienciaComponent,
+    NeweducacionComponent,
+    EditeducacionComponent
   ],
   imports: [
     FormsModule,
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     NgCircleProgressModule.forRoot({}),
     HttpClientModule
---
+++ proyectoFinal_LedesmaAnahi/FrontEnd/src/app/model/experiencia.ts
@@ -0,0 +1,11 @@
+export class Experiencia {
+    id? : number;
+    nombreE : string;
+    descripcionE : string;
+   
+    constructor(nombreE: string, descrpcionE: string){
+      this.nombreE = nombreE;
+      this.descripcionE = descrpcionE;
+   }
+   
+}

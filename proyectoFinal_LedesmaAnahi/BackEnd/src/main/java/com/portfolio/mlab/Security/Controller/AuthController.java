0fe7e1502bf2eeb92d7a72dea1e1162a58838b4c
Anahi <cosmos.anahi@gmail.com>
2022-10-29 20:13:35 -0300

    algunos cambios
    algunos cambios

--- proyectoFinal_LedesmaAnahi/BackEnd/src/main/java/com/portfolio/mlab/Security/Controller/AuthController.java
+++ proyectoFinal_LedesmaAnahi/BackEnd/src/main/java/com/portfolio/mlab/Security/Controller/AuthController.java
@@ -34,7 +34,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin
+//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins ="https://frontendmlab.web.app")
+
 public class AuthController {
 
     @Autowired
--- proyectoFinal_LedesmaAnahi/FrontEnd/src/environments/environment.prod.ts
+++ proyectoFinal_LedesmaAnahi/FrontEnd/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: true
-  //URL:
+  production: true,
+  URL:'https://backendmlab.herokuapp.com/'
 };
